303c077d153c6f18f78eb2b18621878dff0b0830
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-04 14:06:55 +0200

    add search city page

---
+++ lib/pages/search_city.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchCity extends StatelessWidget {
+  const SearchCity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+      
+    );
+  }
+}
