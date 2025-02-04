80bfa270654df05c8e85a1a4117f22818c688818
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-04 16:28:20 +0200

    add wather page

---
+++ lib/pages/wather_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WathrPage extends StatelessWidget {
+  const WathrPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
